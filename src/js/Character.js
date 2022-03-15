de22b9688007074e84b0122c473710bb1ae97c31
Dmitrio <ar-dimasik@ya.ru>
2022-03-15 16:01:40 +0100

    homework oop 1

---
+++ src/js/Character.js
@@ -0,0 +1,8 @@
+export default class Character {
+    constructor(health, level, attack, defence) {
+      this.health = health;
+      this.level = level;
+      this.attack = attack;
+      this.defence = defence;
+    }
+  }
---
+++ src/js/Swordsman.js
@@ -0,0 +1,15 @@
+import Character from './Character';
+
+export default class Swordsman extends Character {
+  constructor(name, type) {
+    super(100, 1, 40, 10);
+    this.name = name;
+    this.type = type;
+    if (this.name.length < 2 || this.name.length > 10) throw new Error('Невалидное имя');
+    if (this.type !== 'Swordsman') throw new Error('Невалидный класс');
+  }
+}
+
+const warrior = new Swordsman('Warrian1234', 'Swordsman');
+
+console.log(warrior);
